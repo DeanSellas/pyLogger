522efa04157f7952192e6a538aeb561c0c05d622
DeanSellas <dgsellas@gmail.com>
2019-10-19 18:56:40 -0500

    small changes to logger main

--- logger.py
+++ logger.py
@@ -12,8 +12,13 @@ class Logger:
             INFO = 1
             NONE = 0
         '''
-        self.level = level
+
         self.setLevel(level)
+
+        self._errorColor = Color.RED
+        self._warnColor = Color.YELLOW
+        self._infoColor = Color.NONE
+
 
     def setLevel(self, level):
         '''
@@ -24,21 +29,25 @@ class Logger:
             INFO = 1
             NONE = 0
         '''
+        if not (0 <= level <= 3):
+            raise ValueError("Level must be an integer between 0 and 3")
+        
         for types in LoggerTypes:
-            if types == level:
+            if types.value == level:
                 self.level = types.value
+                break
 
     def Error(self, text):
         if self.level <= LoggerTypes.ERROR.value:
-            self._write(Color.RED, text)
+            self._write(self._errorColor, text)
 
     def Warn(self, text):
         if self.level <= LoggerTypes.WARN.value:
-            self._write(Color.YELLOW, text)
+            self._write(self._warnColor, text)
 
     def Info(self, text):
         if self.level <= LoggerTypes.INFO.value:
-            self._write(Color.NONE, text)
+            self._write(self._infoColor, text)
 
     def _write(self, color="", text="", reset=True):
         out = color + str(text)
