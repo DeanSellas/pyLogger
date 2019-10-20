b7e4c9f3654ebedc15a092c4753974c1aadf388e
DeanSellas <dgsellas@gmail.com>
2019-10-20 18:02:47 -0500

    Create Errors in Logger

--- logger.py
+++ logger.py
@@ -1,3 +1,5 @@
+import os
+
 from .src.Color import Color
 from .src.LoggerTypes import LoggerTypes
 
@@ -30,24 +32,30 @@ class Logger:
             NONE = 0
         '''
         if not (0 <= level <= 3):
-            raise ValueError("Level must be an integer between 0 and 3")
+            self.Error("Level must be an integer between 0 and 3", ValueError)
         
         for types in LoggerTypes:
             if types.value == level:
                 self.level = types.value
                 break
 
-    def Error(self, text):
+    def Error(self, text, ErrorType=None):
         if self.level <= LoggerTypes.ERROR.value:
-            self._write(self._errorColor, text)
+            self._write(self._errorColor, "ERROR | {}".format(text))
+
+        if ErrorType != None:
+            raise ErrorType(text)
 
     def Warn(self, text):
         if self.level <= LoggerTypes.WARN.value:
-            self._write(self._warnColor, text)
+            self._write(self._warnColor, "WARNING | {}".format(text))
 
     def Info(self, text):
         if self.level <= LoggerTypes.INFO.value:
-            self._write(self._infoColor, text)
+            self._write(self._infoColor, "INFO | {}".format(text))
+
+    def Clear(self):
+        os.system('cls')
 
     def _write(self, color="", text="", reset=True):
         out = color + str(text)
