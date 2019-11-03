9ea8c313a819f3bdecdcaf1008f2a18d349bc1b0
DeanSellas <dgsellas@gmail.com>
2019-11-02 20:24:03 -0500

    ability to save log to file

--- logger.py
+++ logger.py
@@ -1,11 +1,12 @@
 import os
 
+from datetime import datetime as dt
 from .src.Color import Color
 from .src.LoggerTypes import LoggerTypes
 
 class Logger:
     
-    def __init__(self, level = 1):
+    def __init__(self, level = 1, filePath=""):
         '''
         LEVEL : Type of log the logger will print to console
             
@@ -14,7 +15,10 @@ class Logger:
             INFO = 1
             NONE = 0
         '''
-
+        self.toFile = False
+        self.filePath = filePath
+        self.fileName = "{}_{}.txt".format("log", dt.now().strftime('%m%d%y'))        
+        
         self.setLevel(level)
 
         self._errorColor = Color.RED
@@ -22,6 +26,19 @@ class Logger:
         self._infoColor = Color.NONE
         self._inputColor = Color.BLUE
 
+        self.toFile = self._fileChecks()
+
+    def _fileChecks(self):
+        if self.filePath == "" or self.filePath == None:
+            return False
+            
+        if(not os.path.isdir(self.filePath)):
+            r = self.Input("{} Does Not Exist Would You Like To Create It? [Y/n]".format(self.filePath))
+            if r.lower() == 'n':
+                self.Warn("{} Does Not Exist!".format(self.filePath))
+                return False
+            os.mkdir(self.filePath)
+        return True
 
     def setLevel(self, level):
         '''
@@ -66,15 +83,15 @@ class Logger:
         self.Info("Console Cleared")
         os.system('cls')
 
-    def _write(self, color="", text="", reset=True, toConsole=True, toFile=True):
+    def _write(self, color="", text="", reset=True, toConsole=True):
         out = color + str(text)
         
         if toConsole:
             print(out)
 
-        if toFile:
-            #TODO Implent log exporter
-            pass
+        if self.toFile:
+            with open(os.path.join(self.filePath,self.fileName), 'a') as logFile:
+                logFile.write(text+'\n')
 
         if reset:
-            print(Color.RESET)
+            print(Color.RESET, end="")
